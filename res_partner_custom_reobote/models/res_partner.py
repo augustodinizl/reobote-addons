a7e795eaf0f9066b7113069540570bdce0c57da8
Augusto D. Lisbôa <augusto.diniz.l@gmail.com>
2025-01-09 21:15:57 -0300

    fix 5

--- res_partner_custom_reobote/models/res_partner.py
+++ res_partner_custom_reobote/models/res_partner.py
@@ -6,29 +6,32 @@ class ResPartner(models.Model):
     nome_curto_cliente = fields.Char(string="Nome da Categoria")
     categoria_produto_id = fields.Many2one('product.category', string="Categoria de Produto")
 
-    @api.model
-    def create(self, vals):
+    @api.model_create_multi
+    def create(self, vals_list):
         """
         Sobrescreve o método create para criar uma subcategoria de produto
         diretamente no campo categoria_produto_id do parceiro.
         """
-        partner = super(ResPartner, self).create(vals)
+        partners = super(ResPartner, self).create(vals_list)
+        
+        self.flush_model(partners) # Força o commit no banco
 
-        if partner.is_company and partner.customer_rank > 0:
-            product_category = self.env['product.category'].search([
-                ('name', '=', 'Produto Finalizado')
-            ], limit=1)
+        for partner, vals in zip(partners, vals_list):
+            if partner.is_company and partner.customer_rank > 0 and vals.get('nome_curto_cliente'):
+                product_category = self.env['product.category'].search([
+                    ('name', '=', 'Produto Finalizado')
+                ], limit=1)
 
-            if not product_category:
-                raise ValueError(_("Categoria 'Produto Finalizado' não encontrada. Crie-a primeiro."))
+                if not product_category:
+                    raise ValueError(_("Categoria 'Produto Finalizado' não encontrada. Crie-a primeiro."))
 
-            # Cria a subcategoria diretamente no campo categoria_produto_id:
-            partner.write({
-                'categoria_produto_id': self.env['product.category'].create({
-                    'name': partner.nome_curto_cliente,
-                    'parent_id': product_category.id,
-                    'code_prefix': partner.nome_curto_cliente,
-                }).id
-            })
+                # Cria a subcategoria:
+                new_category = self.env['product.category'].create({
+                        'name': vals.get('nome_curto_cliente'),
+                        'parent_id': product_category.id,
+                        'code_prefix': vals.get('nome_curto_cliente'),
+                    })
+                
+                partner.write({'categoria_produto_id': new_category.id})
 
-        return partner
+        return partners
